35f3d58b040af84de5e1b5435b4160d47a4584f1
Ben <pauleybj@gmail.com>
2021-04-29 15:27:23 +0100

    fixed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,16 +13,19 @@ const FILES_TO_CACHE = [
   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
+// install
 self.addEventListener("install", function (evt) {
+  // pre cache all static assets
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
   self.skipWaiting();
 });
 
+// activate
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then((keyList) => {
@@ -40,28 +43,38 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
+// fetch
 self.addEventListener("fetch", function (evt) {
-  // The following code caches responses for requests
-  if (evt.request.url.includes("./routes/api.js")) {
+  if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches
         .open(DATA_CACHE_NAME)
         .then((cache) => {
           return fetch(evt.request)
             .then((response) => {
+              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
+
               return response;
             })
             .catch((err) => {
+              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         })
-        .catch((err) => {
-          console.log(err);
-        })
+        .catch((err) => console.log(err))
     );
+
     return;
   }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
 });
